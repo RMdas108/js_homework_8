974203cb5486e927614c4635ade6dbc74965c130
RMdas108 <rmanohardas@gmail.com>
2023-08-24 18:27:20 +0300

    зроблено включно до 5 завдання

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-//Завдання 1. 
+// Завдання 1.
 "use strict";
 // let text_1_1 = prompt('Введіть перший текст...');
 // let text_1_2 = prompt('Введіть другий текст...');
 // console.log(`Завдання 1.\n${Math.sign(text_1_1.length - text_1_2.length)}`);
 
-//Завдання 2. 
-// let text_2 = prompt('Введіть текст...');
-// console.log(`Завдання 2.\n${text_2.split("").reverse().join("")}`);
+// // Завдання 2.
+// // let text_2 = prompt('Введіть текст...');
+// // console.log(`Завдання 2.\n${text_2.split("").reverse().join("")}`);
 
-//Завдання 3.
-let inputP = prompt('Введіть слово. Подивимось чи є воно паліндромом?');
-let finalWord = inputP.split('').reverse().join('');
-if (inputP === finalWord) {
-  console.log(`${inputP} є паліндромом.`);
-} else {
-  console.log(`${inputP} не є паліндромом.`);
-}
-//Завдання 4.
+// // Завдання 3.
+// let inputP = prompt('Введіть слово. Подивимось чи є воно паліндромом?');
+// let finalWord = inputP.split('').reverse().join('');
+// if (inputP === finalWord) {
+//   console.log(`${inputP} є паліндромом.`);
+// } else {
+//   console.log(`${inputP} не є паліндромом.`);
+// }
+// // Завдання 4.
+// let spam = [
+//   "100% безкоштовно",
+//   "збільшення продажів",
+//   "тільки сьогодні",
+//   "не видаляйте",
+//   "xxx",// латиниця
+//   "ххх",// кирилиця
+// ];
+// let userInput = prompt("Введіть, будь ласка, текст...");
+// let result  = false;
+// for (let i = 0; i < spam.length; i++) {
+//   if (userInput === spam[i]) {
+//     result = true;
+//     break;
+//   } 
+// }
+// console.log(result);
+// Завдання 5.
+const vowels = "аеиіоуяюєїАЕИІОУЯЮЄЇ";
+let text = prompt('Введіть будь-який рядок...');
